6c35bb8501273143248df592301d960199629e1a
FadilArfat <62172926+FadilArfat@users.noreply.github.com>
2023-02-16 09:50:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from flask import Flask, jsonify
-import os
+from email import message
+import pickle
+import joblib
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/',methods=['POST', 'GET'])
+def home():
+  feature = open('vect.pkl', 'rb')
+  model = open('iris.pkl', 'rb')
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+  vec = joblib.load(feature)
+  rbf = joblib.load(model)
+
+  if request.method == 'POST':
+    inp = request.form['inp']
+    data = [inp]
+    vect = vec.transform(data)
+    my_prediction = rbf.predict(vect)[0]
+    if my_prediction == 1:
+        return render_template('home.html', message = "Sentimen negatif 😡")
+    else:
+        return render_template('home.html', message = 'sentimen positif 🙂')
+    
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
